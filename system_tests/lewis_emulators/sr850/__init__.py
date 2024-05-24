c3060db344e9e4d27d2d5bdcbe0617edb250f4ef
vux62295 <vux62295@ndw2633.isis.cclrc.ac.uk>
2024-05-24 14:37:17 +0100

    Add Lewis emulator

---
+++ system_tests/lewis_emulators/sr850/__init__.py
@@ -0,0 +1,5 @@
+from .device import SimulatedSr850
+from ..lewis_versions import LEWIS_LATEST
+
+framework_version = LEWIS_LATEST
+__all__ = ['SimulatedSr850']
---
+++ system_tests/lewis_emulators/sr850/interfaces/__init__.py
@@ -0,0 +1,3 @@
+from .stream_interface import Sr850StreamInterface
+
+__all__ = ['Sr850StreamInterface']
