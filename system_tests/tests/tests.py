67c347d0d57a4677ed0707985df541ce27682d03
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-08-05 13:55:25 +0100

    Run ruff safe fixes

--- system_tests/tests/tests.py
+++ system_tests/tests/tests.py
@@ -1,10 +1,10 @@
 import unittest
 
+from parameterized import parameterized
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
 from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
-from parameterized import parameterized
 
 DEVICE_PREFIX = "SR850_01"
 
